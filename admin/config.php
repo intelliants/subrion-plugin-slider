bdd76dc8ca9c6970c140945392d58bd5b571a0dc
Vladimir <semchishin.vladimir@gmail.com>
2018-02-27 11:57:47 +0600

    Fixes Added

--- admin/config.php
+++ admin/config.php
@@ -24,7 +24,7 @@
  *
  ******************************************************************************/
 
-$iaSlider = $iaCore->factoryModule('slider','slider',iaCore::ADMIN);
+$iaSlider = $iaCore->factoryModule('slider', 'slider', iaCore::ADMIN);
 $iaDb->setTable(iaSlider::getTable());
 
 $allowedAction = ['add_block', 'remove_block', 'save_block'];
